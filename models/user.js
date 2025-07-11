8d58e216bb239b4cfe16f11f6ac90b34dfa45f0e
UegakiYota <Yota.Uegaki@jp.ricoh.com>
2025-07-11 15:34:01 +0900

    細かい調整

--- models/user.js
+++ models/user.js
@@ -13,6 +13,16 @@ const userSchema = new Schema({
 userSchema.plugin(passportLocalMongoose, {
     errorMessages: {
         UserExistsError: 'このユーザはすでに存在します。',
+        IncorrectPasswordError: 'パスワードが間違っています。',
+        MissingPasswordError: 'パスワードが入力されていません。',
+        MissingUsernameError: 'ユーザ名が入力されていません。',
+        IncorrectUsernameError: 'ユーザ名が間違っています。',
+        UserNotFoundError: 'ユーザが見つかりません。',
+        AttemptTooSoonError: 'ログイン試行が早すぎます。しばらく待ってから再試行してください。',
+        TooManyAttemptsError: 'ログイン試行が多すぎます。しばらく待ってから再試行してください。',
+        NoSaltValueStoredError: 'パスワードのハッシュ化に失敗しました。パスワードのソルト値が保存されていません。',
+        InvalidSalt: '無効なソルト値です。パスワードのハッシュ化に失敗しました。',
+        InvalidHash: '無効なハッシュ値です。パスワードのハッシュ化に失敗しました。',
     }
 });//これでユーザスキーマにパスポートが適用される
 
